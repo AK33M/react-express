58b31e1069697779e54e4938cb66f0d3266e0c11
AK33M <akeemtaiwo@outlook.com>
2017-07-17 08:02:27 +0100

    module 1

---
+++ app/components/GroceryItemList.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+class GroceryItemList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Grocery Listify
+                </h1>
+            </div>
+        );
+    }
+}
+
+module.exports = GroceryItemList;
